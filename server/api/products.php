cef5ddc16758c4517ddacf8217a0fdba606c084f
ernest hubbard <ernhub89@gmail.com>
2019-11-18 14:05:27 -0800

    view data backend

---
+++ server/api/products.php
@@ -0,0 +1,17 @@
+<?php
+if ($request['method'] === 'GET') {
+  $link = get_db_link();
+  $query = 'SELECT p.`productid`, p.`name`, p.`price`, p.`shortDescription` FROM `products` AS p';
+  $result = mysqli_query($link, $query);
+  mysqli_num_rows($result);
+  mysqli_affected_rows($db_params);
+  $output = [];
+  while($row = mysqli_fetch_assoc($result)){
+    $output[] = $row;
+  }
+  $response['body'] = [
+    'message' => $output
+  ];
+  send($response);
+}
+?>
