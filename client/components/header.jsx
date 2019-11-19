ba357e725049936891ff3f32752b8c9f9c505268
ernest hubbard <ernhub89@gmail.com>
2019-11-19 09:56:32 -0800

    updated

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <nav className="mb-3 navbar navbar-expand-sm navbar-light col-xs-12 w-100 bg-success">
-        <div className="container">
-          <a className="navbar-brand" href="#"><i className="fas fa-dollar-sign"></i> Wicked Sales</a>
-        </div>
-      </nav>
-    );
-  }
+export default function Header() {
+  return (
+    <nav className="mb-3 navbar navbar-expand-sm navbar-light col-xs-12 w-100 bg-success">
+      <div className="container">
+        <a className="navbar-brand" href="#"><i className="fas fa-dollar-sign"></i> Wicked Sales</a>
+      </div>
+    </nav>
+  );
 }
-
-export default Header;
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 
-class ProductListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="card col-xs-3 col-xl-3 col-l-3 col-m-3 h-25 m-2">
-        <div className="h-50">
-          <img className="card-img-top m-auto image-height" src={this.props.image} alt="" />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">{this.props.name}</h5>
-          <p>${this.props.price}</p>
-          <p className="card-text">{this.props.desc}</p>
-          <button className="btn btn-primary text-center ml-5">Add to Cart</button>
-        </div>
+export default function ProductListItem(props) {
+  return (
+    <div className="card col-xs-3 col-xl-3 col-l-3 col-m-3 h-25 m-2">
+      <div className="h-50">
+        <img className="card-img-top m-auto image-height" src={props.image} alt="" />
       </div>
-    );
-  }
+      <div className="card-body">
+        <h5 className="card-title">{props.name}</h5>
+        <p>${props.price}</p>
+        <p className="card-text">{props.desc}</p>
+        <button className="btn btn-primary text-center ml-5">Add to Cart</button>
+      </div>
+    </div>
+  );
 }
-
-export default ProductListItem;
