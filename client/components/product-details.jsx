4809c9ea811b6b9a374c1ecd1d452176d1b202e1
ernest hubbard <ernhub89@gmail.com>
2019-11-19 16:57:56 -0800

    fixed card

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -15,13 +15,15 @@ class ProductDetails extends React.Component {
     return (
       <div className="card col-xs-3 col-xl-6 col-l-3 col-m-3 h-25 m-2">
         <p onClick={() => this.props.setView('catalog', {})}>{sentence}</p>
-        <div className="h-50">
-          <img className="card-img-top m-auto image-height" src={currentId.image} alt="" />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">{currentId.name}</h5>
-          <p>${currentId.price}</p>
-          <p className="card-text">{currentId.shortDescription}</p>
+        <div className="h-50 col-xs-12 d-flex flex-wrap">
+          <div className="col-xs-4 col-xl-4">
+            <img className=" image-height col-xs-3" src={currentId.image} alt="" />
+          </div>
+          <div className="card-body d-flex flex-wrap col-xl-8">
+            <h3 className="p-0 card-title col-xl-12">{currentId.name}</h3>
+            <h5>${currentId.price}</h5>
+            <p className="card-text">{currentId.shortDescription}</p>
+          </div>
           <p className="card-text">{currentId.longDescription}</p>
         </div>
       </div>
