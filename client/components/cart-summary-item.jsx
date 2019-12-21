52283cd4f90860e78df777fcbc675e267be5ae9e
Ernest Hubbard <ernhub89@gmail.com>
2019-12-21 22:34:47 +0000

    fixed broken image

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -10,7 +10,7 @@ export default function CartSummaryItem(props) {
     <div className="card col-10 mb-4 m-auto">
       <div className="col-xs-12">
         <div className="col-xs-4 col-xl-4 d-inline-block">
-          <img className="image-height col-xs-3" src={image} alt="" />
+          <img className="image-height image-width col-xs-3" src={image} alt="" />
         </div>
         <div className="card-body d-inline-block p-0 col-xl-6 col-xs-6">
           <h3 className="p-0 card-title">{name}</h3>
